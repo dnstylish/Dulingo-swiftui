f1c7941c8b4da43f406ea574e03097e76d5644e4
Nguyên Trần <dnstylish@gmail.com>
2022-04-04 19:13:59 +0700

    Initial Commit

---
+++ Shared/DulingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DulingoApp.swift
+//  Shared
+//
+//  Created by Yuan on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DulingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
